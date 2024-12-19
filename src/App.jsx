b9bd7ac6e0ad6ece9a8c136f62d2354de2f08af1
Your Name <adityaraj642003@gmail.com>
2024-12-20 01:34:38 +0530

    Project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from "react";
+import data from './data'
+import Tours from "./components/Tours";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [tours, setTours] = useState(data);
+
+  function removeTour(id) {
+    const newTours = tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+  }
+
+  if(tours.length === 0) {
+    return (
+        <div className="refresh bg-black text-blue-300">
+          <h2>No Tours Left</h2>
+          <button className="btn-white rounded-xl" onClick={() => setTours(data)}>
+            Refresh
+          </button>
+        </div>
+    );
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App bg-black text-blue-200">
+      <Tours tours={tours} removeTour={removeTour}></Tours>
+    </div>
   )
-}
+};
 
-export default App
+export default App;
